7f5bc9699e3a4923ab4fe06d340a1f49b441da53
Claudio Maccari <claudio@jitidea.net>
2017-01-14 22:40:44 +0100

    consul with 2 static http servers

---
+++ index.js
@@ -0,0 +1,36 @@
+var http = require('http');
+var consul = require('consul')();
+
+
+var makeServiceCall = function (host, port) {
+	var options = {
+		host: host,
+		port: port,
+		path: '/index.json'
+	};
+	callback = function (response) {
+		var str = '';
+
+		//another chunk of data has been recieved, so append it to `str`
+		response.on('data', function (chunk) {
+			str += chunk;
+		});
+
+		//the whole response has been recieved, so we just print it out here
+		response.on('end', function () {
+			console.log(str);
+		});
+	}
+	http.request(options, callback).end();
+}
+
+consul.catalog.service.list(function (err, result) {
+	Object.keys(result).map(function (service) {
+		if (service != 'consul') {
+			consul.catalog.service.nodes(service, function (err, result) {
+				makeServiceCall(result[0].Address, result[0].ServicePort);
+			});
+		}
+	})
+});
+
